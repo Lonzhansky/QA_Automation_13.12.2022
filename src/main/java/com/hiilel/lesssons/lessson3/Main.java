dc68ae3559ad1b980e4fbb068618c3e3e06c10ed
Denys Lonzhanskyi <lonzhansky@gmail.com>
2022-12-23 20:38:36 +0200

    test

--- src/main/java/com/hiilel/lesssons/lessson3/Main.java
+++ src/main/java/com/hiilel/lesssons/lessson3/Main.java
@@ -3,5 +3,9 @@ package com.hiilel.lesssons.lessson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
     }
 }
