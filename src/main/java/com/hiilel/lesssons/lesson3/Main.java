5d6af83818c3c97b3a962ea65ef767e72f49da4b
Lonzhansky <89276411+Lonzhansky@users.noreply.github.com>
2022-12-23 20:49:40 +0200

    Update Main.java
    from github

--- src/main/java/com/hiilel/lesssons/lesson3/Main.java
+++ src/main/java/com/hiilel/lesssons/lesson3/Main.java
@@ -3,13 +3,6 @@ package com.hiilel.lesssons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git!");
-        System.out.println("Hello git!");
-        System.out.println("Hello git!");
-        System.out.println("Hello git!");
-        System.out.println("Hello git!");
-        System.out.println("Hello git!");
-        System.out.println("Hello git!");
-        System.out.println("Hello git!");
-        System.out.println("Hello git!");
+        System.out.println("Hello Java!");
     }
 }
