eb2cc8e6c3b4a436da24b944d0303e6d23e03c07
Denys Lonzhanskyi <lonzhansky@gmail.com>
2022-12-23 20:47:05 +0200

    test 3

--- src/main/java/com/hiilel/lesssons/lessson3/Main.java
+++ src/main/java/com/hiilel/lesssons/lessson3/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
+        System.out.println("Hello git!");
     }
 }
